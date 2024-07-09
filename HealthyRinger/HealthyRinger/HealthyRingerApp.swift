673837442dde0edcd788106e01cfb1768ddedea6
28maryshev <trustoviy@gmail.com>
2024-07-09 20:09:03 +0300

    App created

---
+++ HealthyRinger/HealthyRinger/HealthyRingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyRingerApp.swift
+//  HealthyRinger
+//
+//  Created by Владимир Марышев on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HealthyRingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
