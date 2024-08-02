9dabf65edd9fcf646a6281f8b0443763beffa7e8
28maryshev <trustoviy@gmail.com>
2024-08-02 23:59:09 +0300

    soundView

---
+++ HealthyRinger/HealthyRinger/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  HealthyRinger
+//
+//  Created by Владимир Марышев on 02.08.2024.
+//
+
+import Foundation
+
+struct SoundModel: Identifiable {
+    let id = UUID()
+    let name: String
+}
