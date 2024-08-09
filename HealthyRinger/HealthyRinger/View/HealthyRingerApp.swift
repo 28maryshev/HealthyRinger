b344b99ae516a82b96645cf846fc22a9ea67e515
28maryshev <trustoviy@gmail.com>
2024-08-09 23:15:29 +0300

    Buttons

--- HealthyRinger/HealthyRinger/View/HealthyRingerApp.swift
+++ HealthyRinger/HealthyRinger/View/HealthyRingerApp.swift
@@ -2,13 +2,22 @@ import SwiftUI
 
 @main
 struct HealthyRingerApp: App {
+//    init() {
+//        UITabBar.appearance().barTintColor = UIColor(Color("BackgroundColorSet"))
+//        let appearance = UINavigationBarAppearance()
+//        appearance.configureWithOpaqueBackground()
+//        appearance.backgroundColor = UIColor(Color("BackgroundColorSet"))
+//        
+//        UINavigationBar.appearance().standardAppearance = appearance
+//        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+//    }
     
     var body: some Scene {
         WindowGroup {
             let alarmViewModel = AlarmViewModel()
             let wakeUpIntervalData = WakeUpModel()
             let soundSettingsViewData = SoundSettingsViewModel()
-                     
+            
             HomeView(
                 alarmViewModel: alarmViewModel,
                 wakeUpIntervalData: wakeUpIntervalData,
